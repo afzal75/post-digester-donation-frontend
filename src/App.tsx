21575049d87e2bf59c0a91e3f4309b2a050c0abf
afzal75 <afzalbhuiyan16@gmail.com>
2024-03-15 00:27:06 +0600

    project setup successfully done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-6xl uppercase underline">Relife Donation</h1>
+      <Button>Jerin</Button>
+    </>
+  )
+}
+
+export default App
