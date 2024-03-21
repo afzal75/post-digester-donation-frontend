72d305e426cab713168840853126248aac5a433c
afzal75 <afzalbhuiyan16@gmail.com>
2024-03-21 14:24:53 +0600

    project setup successfully done

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
     reducerPath: "baseApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://relife-digester-donation.vercel.app/api/v1",
+        baseUrl: "http://localhost:5173/api/v1",
     }),
     tagTypes: ["Donations"],
     endpoints: () => ({}),
